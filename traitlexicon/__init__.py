67f0a199582c241e0d8b0ea1908e3f5733cad325
Adhityo Wicaksono <107833553+adhitwicaksono@users.noreply.github.com>
2026-05-19 11:43:13 +0700

    Bump version to 0.2.0 and enhance module functionality
    Updated version number and added new functions to the module.

--- traitlexicon/__init__.py
+++ traitlexicon/__init__.py
@@ -8,10 +8,13 @@ candidate gene/pathway module definitions. It is designed as an upstream
 interpretation layer for downstream tools such as PhenoSieve.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .loader import load_yaml_file, find_yaml_files, load_trait_entries, load_module_definitions
 from .resolver import resolve_trait_phrase
+from .indexer import build_trait_index, build_module_index, find_trait, find_module
+from .exporter import module_to_phenosieve_export, export_module_to_file
+from .scaffold import make_trait_template, make_module_template, write_yaml_template
 
 __all__ = [
     "__version__",
@@ -20,4 +23,13 @@ __all__ = [
     "load_trait_entries",
     "load_module_definitions",
     "resolve_trait_phrase",
+    "build_trait_index",
+    "build_module_index",
+    "find_trait",
+    "find_module",
+    "module_to_phenosieve_export",
+    "export_module_to_file",
+    "make_trait_template",
+    "make_module_template",
+    "write_yaml_template",
 ]
